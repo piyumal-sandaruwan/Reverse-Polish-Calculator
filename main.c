b33d14e0210a98264a31e6c80ca2acceb1bae293
piyumal-sandaruwan <piyumalsandaruwanb@gmail.com>
2026-04-25 01:00:00 +0530

    stack and the top pointer

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
-}
+#define MAX_SIZE 100
+
+/* Global variables for the stack and the top pointer */
+double data[MAX_SIZE];
+int top = -1;
